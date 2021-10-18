c3dd571d2ecb385aad4bb99f96f7856b00065e01
MariaCassese <62397084+MariaCassese@users.noreply.github.com>
2021-10-18 18:20:15 +0200

    Update activation_functions.py

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-def linear(input:np.ndarray):
+def linear(input: np.ndarray):
+    """ linear activation function """
     return input
 
-activation_functions = { 'linear': linear, ''
-}
+def relu(input: np.ndarray):
+    """ ReLU activation function """
+    return np.maximum(input, 0)
+
+def sigmoid(input: np.ndarray):
+    """ Sigmoid activation function """
+    ones = [1.] * len(input)
+    return np.divide(ones, np.add(ones, np.exp(-input)))
+
+activation_functions = { 'linear': linear, 'relu': relu, 'sigmoid':sigmoid}
