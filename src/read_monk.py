6d12c9b1a5056ef262e8dc9d603e29ef4aea9ce3
Luca Moroni <terraformer.144@gmail.com>
2021-10-22 23:05:29 +0200

    created three different functs one for each dataset

--- src/read_monk.py
+++ src/read_monk.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
-def read_monk():
+def read_monk_1():
     monk1_train = pd.read_csv('../datasets/Monks/monks-1.train', sep=' ')
-    monk2_train = pd.read_csv('../datasets/Monks/monks-2.train', sep=' ')
-    monk3_train = pd.read_csv('../datasets/Monks/monks-3.train', sep=' ')
     monk1_test = pd.read_csv('../datasets/Monks/monks-1.test', sep=' ')
+
+    return monk1_train, monk1_test
+
+def read_monk_2():
+    monk2_train = pd.read_csv('../datasets/Monks/monks-2.train', sep=' ')
     monk2_test = pd.read_csv('../datasets/Monks/monks-1.test', sep=' ')
+
+    return monk2_train, monk2_test
+
+def read_monk_3():
+    monk3_train = pd.read_csv('../datasets/Monks/monks-3.train', sep=' ')
     monk3_test = pd.read_csv('../datasets/Monks/monks-1.test', sep=' ')
 
-    return monk1_train, monk1_test, monk2_train, monk2_test, monk3_train, monk3_test
-
+    return monk3_train, monk3_test
