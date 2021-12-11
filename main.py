317bc1757a3569a3bf36ed21dab62ee41ac43a98
Simone Manti <manti998@gmail.com>
2021-12-11 16:26:07 +0100

    Small changes on main.py to set up the grid search for randomized on monk1

--- main.py
+++ main.py
@@ -16,21 +16,21 @@ from sklearn.model_selection import train_test_split
 
 def build_model(hidden_neurons):
 
-    nn = RandomizedNetwork(10,
+    nn = RandomizedNetwork(17,
     [
     RandomizedLayer(hidden_neurons, "relu"),
-    RandomizedLayer(2, "linear")])
+    RandomizedLayer(1, "linear")])
 
     nn.compile(loss=MSE())
 
     return nn
 
 if __name__ == '__main__':
-    X, y = read_cup()
+    train_x, test_x, train_y, test_y = read_monk(1)
     
-    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.10)
+    #train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.10)
 
-    best_params = grid_search_cv(build_model, (train_x, train_y), {"hidden_neurons":[600], "lambda_":[10]}, k_folds=5, direct=True)
+    best_params = grid_search_cv(build_model, (train_x, train_y), {"hidden_neurons":[3500, 4000], "lambda_":[0.01, 0.1, 1]}, k_folds=5, direct=True)
     
     print(best_params)
 
@@ -41,4 +41,8 @@ if __name__ == '__main__':
     loss_tr, loss_vl = model.direct_training((train_x, train_y), **best_params_training)
 
     print(f"MEE error : {model_loss(model, MEE(), test_x, test_y)}")
+
+
+    print(f"training accuracy: {model_accuracy(model, train_x, train_x, threshold = 0.5)}")
+    print(f"test accuracy: {model_accuracy(model, test_x, test_y, threshold = 0.5)}")
 
